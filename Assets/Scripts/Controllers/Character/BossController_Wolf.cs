c9316a41ae0ea6004cd61606e378d8ea3148d098
koexe <aicom5448@gmail.com>
2024-12-10 23:16:19 +0900

    Update

---
+++ Assets/Scripts/Controllers/Character/BossController_Wolf.cs
@@ -0,0 +1,280 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController1 : NPCController
+{
+    enum BossStates
+    {
+        Dialog,
+        InPattern,
+        EndPattern,
+        Stun,
+    }
+
+    [Header("컴포넌트")]
+    [SerializeField] BossStates state;
+    [SerializeField] Collider2D coll2d;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] Animator animator;
+
+    [SerializeField] Vector3 basicAttackOffset;
+    [SerializeField] LayerMask playerLayerMask;
+
+    [Header("보스 패턴 설정")]
+    [SerializeField] bool isCollisionEnabled;
+    [SerializeField] bool currentMoveArrow;
+
+    [SerializeField] float jumpHeight;
+    [SerializeField] float bushMoveSpeed;
+    [SerializeField] float chargeSpeed;
+    [SerializeField] int ambushChargeCount;
+    [SerializeField] Transform[] bushTransform1;
+    [SerializeField] Transform[] bushTransform2;
+
+    [SerializeField] GameObject trapPrefab;
+    [SerializeField] GameObject currentTrapObj;
+
+    [SerializeField] float stunTime;
+
+    Coroutine crCurrentPattern;
+
+    public override void Start()
+    {
+        //Invoke("ShowHunterDialog", 3f);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F1))
+        {
+            this.crCurrentPattern = StartCoroutine(Ambush());
+        }
+    }
+
+
+    public void MoveToPosition(Vector3 _position, float _speed)
+    {
+        this.transform.position = Vector2.MoveTowards(
+                                              this.transform.position,
+                                              _position,
+                                              _speed * Time.fixedDeltaTime);
+    }
+
+    public void ChargeToPosition(Vector3 _position, float _speed)
+    {
+        this.coll2d.enabled = true;
+        this.transform.position = Vector2.MoveTowards(
+                                              this.transform.position,
+                                              _position,
+                                              _speed * Time.fixedDeltaTime);
+    }
+
+    IEnumerator Ambush()
+    {
+        Vector3 t_MovePoint = GetNearestBush();
+        bool t_point = GetNearestBushPoint();
+
+        for (int i = 0; i< this.ambushChargeCount; i ++)
+        {
+            while (Vector3.Distance(this.transform.position, t_MovePoint) > 0.05f)
+            {
+                MoveToPosition(t_MovePoint, this.bushMoveSpeed);
+                yield return new WaitForFixedUpdate();
+            }
+
+            yield return StartCoroutine(SmoothHide());
+
+            if (t_point)
+            {
+                this.transform.position = this.bushTransform1[Random.Range(0, this.bushTransform1.Length)].position;
+                t_MovePoint = this.bushTransform2[Random.Range(0, this.bushTransform2.Length)].position;
+            }
+            else
+            {
+                this.transform.position = this.bushTransform2[Random.Range(0, this.bushTransform2.Length)].position;
+                t_MovePoint = this.bushTransform1[Random.Range(0, this.bushTransform1.Length)].position;
+            }
+
+            this.spriteRenderer.color = Color.white;
+            this.isCollisionEnabled = true;
+
+            yield return new WaitForSeconds(0.5f);
+
+            while (Vector3.Distance(this.transform.position, t_MovePoint) > 0.05f)
+            {
+                ChargeToPosition(t_MovePoint, this.chargeSpeed);
+                yield return new WaitForFixedUpdate();
+            }
+            this.isCollisionEnabled = false;
+            t_MovePoint = GetNearestBush();
+            t_point = GetNearestBushPoint();
+        }
+
+        t_MovePoint = InGameManager.instance.GetPlayerController().transform.position;
+        Vector3 t_StartPos = this.transform.position;
+
+        this.animator.Play("JumpReady");
+        InGameManager.instance.PlayEffect("JumpStart", this.transform.position - new Vector3(0, -1, 0));
+
+        float t_JumpTime = 0.6f;
+        float t_elapsedTime = 0f;
+
+        while(t_elapsedTime < t_JumpTime)
+        {
+            t_elapsedTime += Time.fixedDeltaTime;
+
+            // 이동 경로의 진행 비율
+            float progress = t_elapsedTime / t_JumpTime;
+
+            // 몬스터의 x, y 이동 (직선 경로)
+            Vector3 currentPosition = Vector3.Lerp(t_StartPos, t_MovePoint, progress);
+
+            // 점프 높이 (포물선 형태)
+            float height = Mathf.Sin(Mathf.PI * progress) * this.jumpHeight;
+
+            // 최종 위치에 높이 반영 (z는 실제 높이가 아니라 y축으로 처리)
+            currentPosition.y += height;
+
+            // 몬스터 위치 갱신
+            this.transform.position = currentPosition;
+            yield return new WaitForFixedUpdate();
+        }
+
+        CameraController.instance.TriggerShake(3f);
+
+        var t_player = Physics2D.OverlapCircle(
+                              this.transform.position,
+                              2f,
+                              this.playerLayerMask);
+        if (t_player != null)
+        {
+            t_player.transform.GetComponent<PlayerCharacterController>().AddHp(-1);
+        }
+
+        this.animator.Play("Idle");
+
+        InGameManager.instance.PlayEffect("Jump", this.transform.position - new Vector3(0, -1, 0));
+
+
+        this.crCurrentPattern = null;
+        yield break;
+    }
+
+
+    Vector3 GetNearestBush()
+    {
+        Vector3 t_bushPos = Vector3.zero;
+        float t_distance = 100000f;
+
+        foreach (Transform t in this.bushTransform1)
+        {
+            if (Vector3.Distance(this.transform.position, t.position) < t_distance)
+            {
+                t_distance = Vector3.Distance(this.transform.position, t.position);
+                t_bushPos = t.position;
+            }
+        }
+
+        foreach (Transform t in this.bushTransform2)
+        {
+            if (Vector3.Distance(this.transform.position, t.position) < t_distance)
+            {
+                t_distance = Vector3.Distance(this.transform.position, t.position);
+                t_bushPos = t.position;
+            }
+        }
+
+        return t_bushPos;
+    }
+    bool GetNearestBushPoint()
+    {
+        Vector3 t_bushPos = Vector3.zero;
+        float t_distance = 100000f;
+        bool t_point = false;
+
+        foreach (Transform t in this.bushTransform1)
+        {
+            if (Vector3.Distance(this.transform.position, t.position) < t_distance)
+            {
+                t_distance = Vector3.Distance(this.transform.position, t.position);
+                t_bushPos = t.position;
+                t_point = true;
+            }
+        }
+
+        foreach (Transform t in this.bushTransform2)
+        {
+            if (Vector3.Distance(this.transform.position, t.position) < t_distance)
+            {
+                t_distance = Vector3.Distance(this.transform.position, t.position);
+                t_bushPos = t.position;
+                t_point = false;
+            }
+        }
+
+        return t_point;
+    }
+
+
+    IEnumerator SmoothHide()
+    {
+        while (this.spriteRenderer.color.a != 0)
+        {
+            Color t_color = this.spriteRenderer.color;
+            t_color.a = Mathf.MoveTowards(t_color.a, 0, Time.fixedDeltaTime);
+            this.spriteRenderer.color = t_color;
+            yield return new WaitForFixedUpdate();
+        }
+        yield break;
+    }
+
+
+    public void SetWalkArrow()
+    {
+        if (this.transform.position.x > InGameManager.instance.GetPlayerController().transform.position.x)
+        {
+            if (this.currentMoveArrow)
+            {
+                this.currentMoveArrow = false;
+                this.spriteRenderer.flipX = false;
+                this.basicAttackOffset = -this.basicAttackOffset;
+            }
+        }
+        else
+        {
+            if (!this.currentMoveArrow)
+            {
+                this.currentMoveArrow = true;
+                this.spriteRenderer.flipX = true;
+                this.basicAttackOffset = -this.basicAttackOffset;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (this.isCollisionEnabled)
+                InGameManager.instance.GetPlayerController().AddHp(-1);
+        }
+        if (collision.tag == "BossTrap")
+        {
+            this.state = BossStates.Stun;
+            this.crCurrentPattern =  StartCoroutine(Stun());
+            this.animator.Play("Stun");
+            Destroy(currentTrapObj);
+        }
+        return;
+    }
+    IEnumerator Stun()
+    {
+        this.state = BossStates.Stun;
+        this.isCanInteract = true;
+
+        yield return new WaitForSeconds(this.stunTime);
+        this.state = BossStates.EndPattern;
+        this.isCanInteract = false;
+    }
+}
